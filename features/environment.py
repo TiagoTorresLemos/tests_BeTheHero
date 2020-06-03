0246c5c8743b62d9830e50de95d4d962f3cb2f03
Tiago Lemos <tito-lemos@hotmail.com>
2020-06-03 17:00:59 -0400

    Funcionalidade_Login
    - Entrar sem informar ID
    - Entrar com ID inexistente
    - Verificar o titulo da página
    - Verificar as mensagens de erro.

--- features/environment.py
+++ features/environment.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from time import sleep
 
 def before_all(context):
     context.browser = webdriver.Firefox()
     context.browser.maximize_window()
 
 def after_step(context, step):
-    context.browser.implicitly_wait(3)
+    context.browser.implicitly_wait(5)
+    #sleep(2.5)
+
 
 def after_all(context):
     context.browser.quit()
---
+++ features/steps/login.py
@@ -0,0 +1,48 @@
+from behave import given, when, then # pylint: disable=no-name-in-module
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+
+#Cenario 1
+@given(u'que o fundador acesse a website Be The Hero "{link_web}"')
+def acessaWeb(context, link_web):
+    context.browser.get(link_web)
+
+@then(u'a mensagem "Be The Hero" deverá estar no título')
+def verificar_titulo(context):
+    assert context.browser.title == "Be The Hero"
+
+#Cenario 2
+@when(u'o fundador tenta entrar sem informar o Id')
+def clickBottonEntrar(context):
+    btn_Entrar = context.browser.find_element_by_class_name("button")
+    btn_Entrar.click()
+
+
+@then(u'o fundador visualiza a mensagem "Falha no login, tente novamente."')
+def verificarAlerta_Login(context):
+    sleep(1)
+    msmAlert = context.browser.switch_to.alert
+    msg = msmAlert.text
+    assert msg == "Falha no login, tente novamente."
+    msmAlert.accept()
+
+#Cenario 3
+@then(u'informa o Id "151617"')
+def id_Invalido(context):
+    idText = context.browser.find_element_by_xpath("//input[@value='']")
+    idText.send_keys("151617")
+
+
+@when(u'o fundador tenta entrar')
+def click_Entrar(context):
+    btn_Entrar = context.browser.find_element_by_class_name("button")
+    btn_Entrar.click()
+
+@then(u'a mensagem "Falha no login, tente novamente." é exibida')
+def verificarLogin_Invaldo(context):
+    sleep(1)
+    msmAlert = context.browser.switch_to.alert
+    msg = msmAlert.text
+    assert msg == "Falha no login, tente novamente."
+    msmAlert.accept()
